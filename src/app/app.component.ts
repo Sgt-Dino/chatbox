ae6a272b5e054ba440e54b379e5808606a5dd895
unknown <dylanmarais100@gmail.com>
2020-02-17 08:42:05 +0200

    Update in components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
-import { Component, Pipe, PipeTransform, IterableDiffers } from '@angular/core';
+import { Component, IterableDiffers } from '@angular/core';
 // import { AngularFireDatabase } from '@angular/fire/database';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Observable } from 'rxjs';
-
 
 
 @Component({
@@ -34,7 +33,6 @@ export class AppComponent {
 
   title = 'chatbox';
 
-
   add(chat: HTMLInputElement) {
     let endDate = Date.now();
     let dif = 2000000000000 - endDate;
@@ -56,27 +54,4 @@ export class AppComponent {
   // ngOnDestroy(){
   //   this.subscription.unsubscribe();
   // }
-}
-
-@Pipe({
-  name: 'reverse',
-  pure: false
-})
-export class ReversePipe implements PipeTransform {
-
-  differs: any;
-  cashed: any;
-  constructor(private _differs: IterableDiffers) {
-    this.differs = this._differs.find([]).create(null);
-  }
-  /**
-   * Takes a value and makes it lowercase.
-   */
-  transform(value, ...args) {
-    const changes = this.differs.diff(value);
-    if (changes) {
-      this.cashed = value.slice().reverse();
-    }
-    return this.cashed;
-  }
 }
