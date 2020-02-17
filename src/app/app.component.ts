363c4fed4ad458d9b5b2b16bbe0d45e750130a22
unknown <dylanmarais100@gmail.com>
2020-02-17 08:35:34 +0200

    latest message shows on top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Pipe, PipeTransform, IterableDiffers } from '@angular/core';
 // import { AngularFireDatabase } from '@angular/fire/database';
 import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Observable } from 'rxjs';
+
 
 
 @Component({
@@ -13,23 +14,34 @@ export class AppComponent {
   mainchat: Observable<any[]>;
   item: AngularFireList<any>;
   user = 'Anonymous';
+  startDate = new Date('2099-02-20T12:01:04.753Z');
   // mainchat: any[];
   // subscription: Subscription;
 
   constructor(db: AngularFireDatabase) {
-        this.mainchat = db.list('/mainchat').valueChanges();
+
+        this.mainchat = db.list('/mainchat', ref => {
+          let q = ref.orderByChild('time');
+          return q;
+        }
+        ).valueChanges();
         this.item = db.list('mainchat');
     // this.subscription = db.list('mainchat').valueChanges().subscribe(mainchat => {
     //       this.mainchat = mainchat;
     //       console.log(this.mainchat);
     //   });
   }
+
   title = 'chatbox';
 
+
   add(chat: HTMLInputElement) {
+    let endDate = Date.now();
+    let dif = 2000000000000 - endDate;
     this.item.push({
       message: chat.value,
-      user: this.user
+      user: this.user,
+      time: dif
     });
     chat.value = '';
   }
@@ -44,4 +56,27 @@ export class AppComponent {
   // ngOnDestroy(){
   //   this.subscription.unsubscribe();
   // }
+}
+
+@Pipe({
+  name: 'reverse',
+  pure: false
+})
+export class ReversePipe implements PipeTransform {
+
+  differs: any;
+  cashed: any;
+  constructor(private _differs: IterableDiffers) {
+    this.differs = this._differs.find([]).create(null);
+  }
+  /**
+   * Takes a value and makes it lowercase.
+   */
+  transform(value, ...args) {
+    const changes = this.differs.diff(value);
+    if (changes) {
+      this.cashed = value.slice().reverse();
+    }
+    return this.cashed;
+  }
 }
